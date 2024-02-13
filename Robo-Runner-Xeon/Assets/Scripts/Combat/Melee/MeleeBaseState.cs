8e9b398bb9a69eba9b38dee408cd0e88bd84bc66
Kuma <n_drolet@hotmail.ca>
2024-02-13 02:54:23 -0500

    Implement basic shooting: Just your good old bullet shooter. Made a one-frame animation to show bullet being fired, reverts standard animations after half second. Currently no firing cooldown. Oh-so-exploitable.

--- Robo-Runner-Xeon/Assets/Scripts/Combat/Melee/MeleeBaseState.cs
+++ Robo-Runner-Xeon/Assets/Scripts/Combat/Melee/MeleeBaseState.cs
@@ -2,82 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class MeleeBaseState : State
 {
-    public float duration;
-    protected Animator animator;
-    protected bool shouldCombo;
-    protected int attackIndex;
+    // public float duration;
+    // protected Animator animator;
+    // protected bool shouldCombo;
+    // protected int attackIndex;
 
 
 
-    protected Collider2D hitCollider;
-    private List<Collider2D> collidersDamaged;
-    private GameObject HitEffectPrefab;
+    // protected Collider2D hitCollider;
+    // private List<Collider2D> collidersDamaged;
+    // private GameObject HitEffectPrefab;
 
-    private float AttackPressedTimer = 0;
+    // private float AttackPressedTimer = 0;
 
-    public override void OnEnter(MeleeController _meleeController)
-    {
-        base.OnEnter(_meleeController);
-        animator = GetComponent<Animator>();
-        collidersDamaged = new List<Collider2D>();
-        hitCollider = GetComponent<ComboCharacter>().hitbox;
-        HitEffectPrefab = GetComponent<ComboCharacter>().Hiteffect;
-    }
+    // public override void OnEnter(MeleeController _meleeController)
+    // {
+    //     base.OnEnter(_meleeController);
+    //     animator = GetComponent<Animator>();
+    //     collidersDamaged = new List<Collider2D>();
+    //     hitCollider = GetComponent<ComboCharacter>().hitbox;
+    //     HitEffectPrefab = GetComponent<ComboCharacter>().Hiteffect;
+    // }
 
-    public override void OnUpdate()
-    {
-        base.OnUpdate();
-        AttackPressedTimer -= Time.deltaTime;
+    // public override void OnUpdate()
+    // {
+    //     base.OnUpdate();
+    //     AttackPressedTimer -= Time.deltaTime;
 
-        if (animator.GetFloat("Weapon.Active") > 0f)
-        {
-            Attack();
-        }
+    //     if (animator.GetFloat("Weapon.Active") > 0f)
+    //     {
+    //         Attack();
+    //     }
 
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            AttackPressedTimer = 2;
-        }
+    //     if (Input.GetMouseButtonDown(0))
+    //     {
+    //         AttackPressedTimer = 2;
+    //     }
 
-        if (animator.GetFloat("AttackWindow.Open") > 0f && AttackPressedTimer > 0)
-        {
-            shouldCombo = true;
-        }
-    }
+    //     if (animator.GetFloat("AttackWindow.Open") > 0f && AttackPressedTimer > 0)
+    //     {
+    //         shouldCombo = true;
+    //     }
+    // }
 
-    public override void OnExit()
-    {
-        base.OnExit();
-    }
+    // public override void OnExit()
+    // {
+    //     base.OnExit();
+    // }
 
-    protected void Attack()
-    {
-        Collider2D[] collidersToDamage = new Collider2D[10];
-        ContactFilter2D filter = new ContactFilter2D();
-        filter.useTriggers = true;
-        int colliderCount = Physics2D.OverlapCollider(hitCollider, filter, collidersToDamage);
-        for (int i = 0; i < colliderCount; i++)
-        {
+    // protected void Attack()
+    // {
+    //     Collider2D[] collidersToDamage = new Collider2D[10];
+    //     ContactFilter2D filter = new ContactFilter2D();
+    //     filter.useTriggers = true;
+    //     int colliderCount = Physics2D.OverlapCollider(hitCollider, filter, collidersToDamage);
+    //     for (int i = 0; i < colliderCount; i++)
+    //     {
 
-            // if (!collidersDamaged.Contains(collidersToDamage[i]))
-            // {
-            //     TeamComponent hitTeamComponent = collidersToDamage[i].GetComponentInChildren<TeamComponent>();
+    //         if (!collidersDamaged.Contains(collidersToDamage[i]))
+    //         {
+    //             TeamComponent hitTeamComponent = collidersToDamage[i].GetComponentInChildren<TeamComponent>();
 
-            //     if (hitTeamComponent && hitTeamComponent.teamIndex == TeamIndex.Enemy)
-            //     {
-            //         GameObject.Instantiate(HitEffectPrefab, collidersToDamage[i].transform);
-            //         Debug.Log("Enemy Has Taken:" + attackIndex + "Damage");
-            //         collidersDamaged.Add(collidersToDamage[i]);
-            //     }
-            // }
-        }
-    }
-
+    //             if (hitTeamComponent && hitTeamComponent.teamIndex == TeamIndex.Enemy)
+    //             {
+    //                 GameObject.Instantiate(HitEffectPrefab, collidersToDamage[i].transform);
+    //                 Debug.Log("Enemy Has Taken:" + attackIndex + "Damage");
+    //                 collidersDamaged.Add(collidersToDamage[i]);
+    //             }
+    //         }
+    //     }
+    // }
 }
